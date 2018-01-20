2a19fe0d32f5eaa3bb279334e1400c4fa8f37ebf
Jason Lukose <jlukose12@gmail.com>
2018-01-20 01:28:23 -0500

    face_recognition

---
+++ facial.py
@@ -0,0 +1,55 @@
+import numpy as np
+import cv2
+import face_recognition
+
+cap = cv2.VideoCapture("testvid.mov")
+# Initialize some variables
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+
+while True:
+    # Grab a single frame of video
+    ret, frame = cap.read()
+
+    # Resize frame of video to 1/4 size for faster face recognition processing
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_small_frame = small_frame[:, :, ::-1]
+
+    # Only process every other frame of video to save time
+    if process_this_frame:
+        # Find all the faces and face encodings in the current frame of video
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        print(face_locations)
+        face_encodings = face_recognition.face_encodings(
+            rgb_small_frame, face_locations)
+
+    process_this_frame = not process_this_frame
+
+    # Display the results
+    for (top, right, bottom, left) in face_locations:
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        crop = frame[top:bottom, left:right]
+        cv2.imshow('cropped', crop)
+        #cv2.waitKey(0)
+
+    # Display the resulting image
+    cv2.imshow('Video', frame)
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
