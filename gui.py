a2b25e25ef888696a6720ea8f4ca584163862906
Jason Lukose <jlukose12@gmail.com>
2018-01-20 18:54:56 -0500

    gui

---
+++ gui.py
@@ -0,0 +1,31 @@
+import Tkinter as tk
+import cv2
+from PIL import Image, ImageTk
+
+width, height = 800, 800
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+root = tk.Tk()
+root.state("zoomed")  #to make it full screen
+
+root.bind('<Escape>', lambda e: root.quit())
+lmain = tk.Label(root)
+lmain2 = tk.Label(root, text="Okay this is it")
+
+lmain.pack(side=tk.LEFT)
+lmain2.pack()
+
+def show_frame():
+    _, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+    img = Image.fromarray(cv2image)
+    imgtk = ImageTk.PhotoImage(image=img)
+    lmain.imgtk = imgtk
+    lmain.configure(image=imgtk)
+    lmain.after(10, show_frame)
+
+show_frame()
+root.mainloop()
