faaedb966efc13631a9e83fc5f1f4c4d459b0367
Jason Lukose <jlukose12@gmail.com>
2018-01-20 02:19:51 -0500

    Facial update

--- facial.py
+++ facial.py
@@ -2,13 +2,25 @@ import numpy as np
 import cv2
 import face_recognition
 
+face_folder = "faces/"
 cap = cv2.VideoCapture("testvid.mov")
+(major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+
+if int(major_ver)  < 3 :
+    fps = cap.get(cv2.cv.CV_CAP_PROP_FPS)
+    print("Frames per second using video.get(cv2.cv.CV_CAP_PROP_FPS): {0}".format(fps))
+else :
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    print("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
+
+
 # Initialize some variables
 face_locations = []
 face_encodings = []
 face_names = []
 process_this_frame = True
-
+frame_count = 0;
+count = 0;
 while True:
     # Grab a single frame of video
     ret, frame = cap.read()
@@ -41,6 +53,9 @@ while True:
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
         crop = frame[top:bottom, left:right]
         cv2.imshow('cropped', crop)
+        cv2.imwrite(face_folder+str(count)+'.jpg', crop)
+        count+=1
+
         #cv2.waitKey(0)
 
     # Display the resulting image
@@ -49,6 +64,8 @@ while True:
     # Hit 'q' on the keyboard to quit!
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+
 
 # Release handle to the webcam
 video_capture.release()
