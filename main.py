851fa096d30838df7f04d2b7fbab1f6b8884ba85
Jason Lukose <jlukose12@gmail.com>
2018-01-21 04:44:57 -0500

    Refresh on button

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ def serve_img(face_index):
         img = Image.fromarray(last_face[0][:,:,::-1])
         # img = Image.new('RGB', ...)
     else:
-        img = img = Image.new('RGB', (800,1280), (255, 0, 255))
+        img = img = Image.new('RGB', (800,800), (255, 0, 255))
     return serve_pil_image(img)
 
 
